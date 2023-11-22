31a4574d4acbbc7d5d8bc7c903b26f3cb7f32e34
jperaino <jim@spatiometrics.com>
2023-11-21 21:56:51 -0800

    add dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSlideToggleModule,
+    DashboardComponent,
+  ],
 })
 export class AppComponent {
   title = 'financial-planner';
